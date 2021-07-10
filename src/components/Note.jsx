631caf7bce6072d5d79d1ed030dfa3dde4ad8485
NIKITHABLN <85407903+NIKITHABLN@users.noreply.github.com>
2021-07-10 13:08:41 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+      <p>
+        {" "}
+        Hi, I made this project during the 7 days free BOotcamp,conducted by
+        <b> SHAPEAI</b>.The instructor during the session was Mr.Shaurya
+        Sinha(Data Anayst Intern at Jio).I got to learn a lot during these 7
+        days and it was an amazing experience learning with SHAPEAI.<br></br>
+        Here's the link for you to watch the sessions as well.<br></br>{" "}
+        <a>
+          {" "}
+          href="https://www.youtube.com/playlist?list=PL7zI8TDRnbuILetcbkthT0p_IzwgRAYbu">
+        </a>
+        <br></br> I got to have hands on experience on:<li>Javascript</li>
+        <li>HTML</li>
+        <li>React.js</li>During these 7 days, and everything was explained from
+        the very basics so that anyone with zero experience on programming can
+        learn.I enjoyed these 7days,you can as well.To register for next free
+        7days bootcamp,visit:
+        <a>href="https://www.shapeai.tech">www.shapeai.tech</a> or followSHAPEAI
+        on:
+        <li>
+          <a> href="https://in.linkedin.com/company/shapeai">Linkedin</a>
+        </li>{" "}
+        <li>
+          <a> href="https://www.instagram.com/shape.ai/?hI=en">Instagram</a>
+        </li>{" "}
+        <li>
+          <a>
+            href="https://www.youtube.com/channel/UCTUvDLTW9meuDXWcbmISPdA">Youtube
+          </a>
+        </li>{" "}
+        <li>
+          <a> href="https://github.com/shapeai">GitHub</a>
+        </li>
+      </p>
+     </div>
+  );
+}
+
+export default Note;
